e915df42a68aa092d7e35ced48ed18d03ac3a4d3
Lukasz Pawluczuk <pawko18@wp.pl>
2014-10-19 16:50:27 +0200

    streching progress

--- src/core/transformations/histogram_stretching.cpp
+++ src/core/transformations/histogram_stretching.cpp
@@ -19,7 +19,52 @@ PNM* HistogramStretching::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    if (image->format() == QImage::Format_Indexed8)
+    {
+        QHash<int, int>::const_iterator i;
+        QHash<int, int>* hash = image->getHistogram()->get(Histogram::LChannel);
+        int min = 0;
+        int max = 0;
+        for (i = hash->constBegin(); i != hash->constEnd(); ++i) {
+            if (i.value() != 0) {
+                min = i.key();
+                break;
+            }
+        }
+
+
+    // tutaj chyba jest zle
+        for (i = hash->constEnd(); i != hash->constBegin(); --i) {
+            if (i.value() != 0) {
+                max = i.key();
+                break;
+            }
+        }
+        for (int x=0; x<width; x++)
+            for (int y=0; y<height; y++)
+            {
+                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+                int v = 255 / (max-min) * (qGray(pixel) - min);
+
+                newImage->setPixel(x, y, v);
+            }
+    }
+    else if (image->format() == QImage::Format_RGB32) {
+//        for (int x=0; x<width; x++)
+//            for (int y=0; y<height; y++)
+//            {
+//                QRgb pixel = image->pixel(x,y); // Getting the pixel(x,y) value
+
+//                int r = qRed(pixel);    // Get the 0-255 value of the R channel
+//                int g = qGreen(pixel);  // Get the 0-255 value of the G channel
+//                int b = qBlue(pixel);   // Get the 0-255 value of the B channel
+
+//                R->insert(r, R->value(r, 0)+1);
+//                G->insert(g, G->value(g, 0)+1);
+//                B->insert(b, B->value(b, 0)+1);
+//            }
+    }
 
     return newImage;
 }
