20549ac1a0f515ac5f762f448161e37ac6eb557b
unknown <nitZ@nitZ-Komputer.(none)>
2014-11-05 13:20:44 +0100

    poprawione zadanie 4

--- src/core/transformations/blur_gaussian.cpp
+++ src/core/transformations/blur_gaussian.cpp
@@ -26,9 +26,9 @@ math::matrix<float> BlurGaussian::getMask(int size, Mode)
 {
     math::matrix<float> mask(size, size);
 
-    for(int x=0; x<size; ++x){
-        for(int y=0; y<size; ++y){
-            mask(x,y)=getGauss(x-radius,y-radius,sigma);
+    for(int x=0; x<size; x++){
+        for(int y=0; y<size; y++){
+            mask(x,y)=this->getGauss(x-radius,y-radius,sigma);
         }
     }
     return mask;
@@ -36,7 +36,6 @@ math::matrix<float> BlurGaussian::getMask(int size, Mode)
 
 float BlurGaussian::getGauss(int x, int y, float sigma)
 {    
-    double sigma_sq_x2 = 2 * pow(sigma,2);
-    return qExp(-(pow(x,2) + pow(y,2)) / sigma_sq_x2) / sigma_sq_x2;
+    return (1 / (2 * M_PI*sigma*sigma))*pow(M_E, (-1)*(x*x + y*y) / (2 * sigma*sigma));
 }
 
--- src/core/transformations/blur_uniform.cpp
+++ src/core/transformations/blur_uniform.cpp
@@ -13,14 +13,12 @@ BlurUniform::BlurUniform(PNM* img, ImageViewer* iv) :
 math::matrix<float> BlurUniform::getMask(int size, Mode)
 {
     math::matrix<float> mask(size, size);
-    Mode mode = RepeatEdge;
 
-    for (int x=0;x<size;++x){
-          for (int y=0;y<size;++y){
-              mask(x,y)=1;
+    for (int x=0;x<size;x++){
+          for (int y=0;y<size;y++){
+              mask(x,y)=1.0;
           }
     }
-    convolute(mask, mode);
 
     return mask;
 }
