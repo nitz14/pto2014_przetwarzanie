037ffa4b3560fa0011d30fdbe8fc0c9346344ce6
nitz14 <nitz@o2.pl>
2014-11-07 19:14:57 +0100

    zadanie 6 jedno dziala

--- src/core/transformations/noise_bilateral.cpp
+++ src/core/transformations/noise_bilateral.cpp
@@ -1,4 +1,5 @@
 #include "noise_bilateral.h"
+#include <cmath>
 
 NoiseBilateral::NoiseBilateral(PNM* img) :
     Convolution(img)
@@ -20,29 +21,81 @@ PNM* NoiseBilateral::transform()
     sigma_d = getParameter("sigma_d").toInt();
     sigma_r = getParameter("sigma_r").toInt();
     radius = sigma_d;
-
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
+    Mode mode = RepeatEdge;
+    QRgb pixel;
+    for(int x=0;x<width;x++){
+        for(int y=0;y<height;y++){
+            if(image->format() == QImage::Format_RGB32){
+               pixel = getPixel(x,y,mode);
+               int r,g,b;
+               r=calcVal(x,y,RChannel);
+               g=calcVal(x,y,GChannel);
+               b=calcVal(x,y,BChannel);
+               //te warunki sa bo cos nie pyklo w getMedian can you look ?
+               if(r>255){
+                   r=255;
+               }
+               if(r<0){
+                   r=0;
+               }
+               if(g>255){
+                   g=255;
+               }
+               if(g<0){
+                   g=0;
+               }
+               if(b>255){
+                   b=255;
+               }
+               if(b<0){
+                   b=0;
+               }
+               //sprawdzic
+               QColor newPixel = QColor(r,g,b);
+               newImage->setPixel(x,y,newPixel.rgb());
+            }
+            if(image->format() == QImage::Format_Mono){
+               pixel = image->pixel(x,y);
+               int v= qGrey(pixel);
+               int temp = calcVal(x,y,LChannel);
+               newImage->setPixel(x,y,temp);
+            }
+        }
+    }
     return newImage;
 }
 
 int NoiseBilateral::calcVal(int x, int y, Channel channel)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
-    return 0;
+    Mode mode = RepeatEdge;
+    int radius = sigma_d;
+    math::matrix<double> win = get(x,y,radius*2+1,channel,mode);
+    int licz,mian,temp,temp2,tempx,tempy;
+    licz=mian=0;
+    tempx = win.rowno();
+    tempy = win.colno();
+    for(int i=0;i<tempx;i++){
+        for(int j=0;j<tempy;j){
+            QPoint p1 = QPoint(i,j);
+            QPoint p2 = QPoint(x,y);
+            int val1,val2;
+            //val1 = I(i,j);
+            //val2 = I(x,y);
+            temp=val1*colorCloseness(val1,val2)*spatialCloseness(p1,p2);
+            temp2=colorCloseness(val1,val2)*spatialCloseness(p1,p2);
+            licz+=temp;
+            mian+=temp2;
+        }
+    }
+    return licz/mian;
 }
 
 float NoiseBilateral::colorCloseness(int val1, int val2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
-    return 0;
+    return pow(M_E,-(pow(val1-val2,2.0)/2*pow(sigma_r,2.0)));
 }
 
 float NoiseBilateral::spatialCloseness(QPoint point1, QPoint point2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
-    return 0;
+    return pow(M_E,-((pow(point1.x() - point2.x(),2.0)+pow(point1.y()-point2.y(),2.0))/2*pow(sigma_d,2.0)));
 }
--- src/core/transformations/noise_median.cpp
+++ src/core/transformations/noise_median.cpp
@@ -16,17 +16,68 @@ PNM* NoiseMedian::transform()
     int height = image->height();
 
     PNM* newImage = new PNM(width, height, image->format());
-
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
+    Mode mode = RepeatEdge;
+    QRgb pixel;
+    int r,g,b,v,tempv;
+    for(int x=0; x<width;x++){
+        for(int y=0; y<height;y++){
+            if(image->format() == QImage::Format_RGB32){
+                pixel = getPixel(x,y,mode);
+                r=getMedian(x,y,RChannel);
+                g=getMedian(x,y,GChannel);
+                b=getMedian(x,y,BChannel);
+                //te warunki sa bo cos nie pyklo w getMedian can you look ?
+                if(r>255){
+                    r=255;
+                }
+                if(r<0){
+                    r=0;
+                }
+                if(g>255){
+                    g=255;
+                }
+                if(g<0){
+                    g=0;
+                }
+                if(b>255){
+                    b=255;
+                }
+                if(b<0){
+                    b=0;
+                }
+                //to mozna wywalic jak naprawimy to nizej
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y,newPixel.rgb());
+            }
+            if(image->format() == QImage::Format_Mono){
+                pixel = image->pixel(x,y);
+                v=qGray(pixel);
+                tempv=getMedian(x,y,LChannel);
+                newImage->setPixel(x,y,tempv);
+            }
+        }
+    }
     return newImage;
 }
 
 int NoiseMedian::getMedian(int x, int y, Channel channel)
 {
     int radius = getParameter("radius").toInt();
-
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
-
-    return 0;
+    Mode mode =	RepeatEdge;
+    int pom,temp;
+    pom = radius*2+1;
+    temp = 0;
+    double tab[100];
+    math::matrix<double> wind = getWindow(x,y,pom,channel,mode);
+    int tempx,tempy;
+    tempx= wind.colno();
+    tempy= wind.rowno();
+    for (int i=0; i<tempx;i++){
+        for (int j=0; j<tempy;j++){
+            tab[temp]=wind(i,j);
+            temp++;
+        }
+    }
+    std::sort(&tab[0],&tab[temp]);
+    return tab[temp/2];
 }
