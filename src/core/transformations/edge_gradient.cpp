2211210df2a92092ee9a99c5dffab6a9fca8dc25
Lukasz Pawluczuk <pawko18@wp.pl>
2014-11-18 21:43:02 +0100

    zadanie 8.1

--- src/core/transformations/edge_gradient.cpp
+++ src/core/transformations/edge_gradient.cpp
@@ -24,8 +24,39 @@ PNM* EdgeGradient::transform()
 {
     PNM* newImage = new PNM(image->width(), image->height(), image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    PNM* imageX = horizontalDetection();
+    PNM* imageY = verticalDetection();
 
+    if (image->format() == QImage::Format_Indexed8)
+    {
+        // Iterate over image space
+        for (int x=0; x<image->width(); x++)
+            for (int y=0; y<image->height(); y++)
+            {
+                QRgb pixelX = imageX->pixel(x,y); // Getting the pixel(x,y) value
+                QRgb pixelY = imageY->pixel(x,y); // Getting the pixel(x,y) value
+
+                int v = (int) sqrt(pow(qGray(pixelX), 2.0) + pow(qGray(pixelY), 2.0));
+                newImage->setPixel(x,y, v);
+            }
+    }
+    else if (image->format() == QImage::Format_RGB32)
+    {
+        // Iterate over image space
+        for (int x=0; x<image->width(); x++)
+            for (int y=0; y<image->height(); y++)
+            {
+                QRgb pixelX = imageX->pixel(x,y); // Getting the pixel(x,y) value
+                QRgb pixelY = imageY->pixel(x,y); // Getting the pixel(x,y) value
+
+                int r = (int) sqrt(pow(qRed(pixelX), 2.0) + pow(qRed(pixelY), 2.0));
+                int g = (int) sqrt(pow(qGreen(pixelX), 2.0) + pow(qGreen(pixelY), 2.0));
+                int b = (int) sqrt(pow(qBlue(pixelX), 2.0) + pow(qBlue(pixelY), 2.0));
+
+                QColor newPixel = QColor(r,g,b);
+                newImage->setPixel(x,y, newPixel.rgb());
+            }
+    }
     return newImage;
 }
 
