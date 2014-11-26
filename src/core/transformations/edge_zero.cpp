a618262aca34e20130fe0d23805c6fd5d95c6ae5
Lukasz Pawluczuk <pawko18@wp.pl>
2014-11-26 17:48:39 +0100

    edge zero progress

--- src/core/transformations/edge_zero.cpp
+++ src/core/transformations/edge_zero.cpp
@@ -21,7 +21,34 @@ PNM* EdgeZeroCrossing::transform()
     double sigma = getParameter("sigma").toDouble();
     int    t     = getParameter("threshold").toInt();
 
+    EdgeLaplaceOfGauss* laplaceOfGauss = new EdgeLaplaceOfGauss(image, supervisor);
+    int v0 = 128;
     PNM* newImage = new PNM(width, height, QImage::Format_Indexed8);
+
+    for (int i=0; i<width; i++)
+        for (int j=0; j<height; j++)
+        {
+            math::matrix<float> mask = laplaceOfGauss->getMask(size, RepeatEdge);
+            float min = 0;
+            float max = 255;
+            for (int x=0; x<size; ++x) {
+                for(int y=0; y<size; ++y) {
+                    if(mask(x,y) < min) {
+                        min = mask(x,y);
+                    }
+                    if(mask(x,y) > max) {
+                        max = mask(x,y);
+                    }
+                }
+            }
+
+            if(min<v0-t && max>v0+t) {
+            } else {
+
+            }
+
+        }
+
 
     qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 
