0f6b3ed8d0298a0e75f3df2dfe7cb29b4de9174a
nitz14 <nitz@o2.pl>
2014-12-09 00:30:46 +0100

    bo sie rozpedzilem

--- src/core/transformations/hough.cpp
+++ src/core/transformations/hough.cpp
@@ -14,10 +14,52 @@ Hough::Hough(PNM* img, ImageViewer* super) :
 }
 
 PNM* Hough::transform()
-{   
+{
     int thetaDensity = getParameter("theta_density").toInt();
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    int width  = image->width(),
+        height = image->height();
 
-    return 0;
+    PNM* tmpImage = ConversionGrayscale(image).transform();
+
+    if(getParameter("skip_edge_detection").toBool() == false){
+        EdgeLaplacian lapl(tmpImage);
+        lapl.setParameter("size", 3);
+        tmpImage = lapl.transform();
+    }
+    int rhoMax = sqrt(width*width+height*height);
+    int thetaSize = 180*thetaDensity;
+    PNM* newImage = new PNM(thetaSize, rhoMax*2+1, QImage::Format_Indexed8);
+    math::matrix<int> hough(thetaSize, rhoMax*2+1);
+    for(int x=hough.rowno()-1; x>=0; x--){
+        for(int y=hough.colno()-1; y>=0; y--){
+            hough(x,y) = 0;
+        }
+    }
+    for(int i=0; i < width; i++){
+        for(int j=0; j < height; j++){
+            int v = qRed(tmpImage->pixel(i, j));
+            if( v > 0){
+                for(int k=0; k < thetaSize; k++){
+                    double theta = (k*M_PI) /(180.0*thetaDensity);
+                    int rho = round(i*cos(theta) + j*sin(theta));
+                    hough(k, rho+rhoMax)++;
+                }
+            }
+        }
+    }
+    int max = 0;
+    for(int x=hough.rowno()-1; x>=0; x--){
+        for(int y=hough.colno()-1; y>=0; y--){
+            if(hough(x,y) > max)
+                max = hough(x,y);
+        }
+    }
+    for(int i=0; i < newImage->width(); i++){
+        for(int j=0; j < newImage->height(); j++){
+            newImage->setPixel(i, j, round(255*hough(i,j)/max));
+        }
+    }
+
+    return newImage;
 }
