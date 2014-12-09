6128d2caa36af3b1c20595ba77ba9b1c0e26c3dc
Lukasz Pawluczuk <pawko18@wp.pl>
2014-12-09 21:37:49 +0100

    progress

--- src/core/transformations/edge_canny.cpp
+++ src/core/transformations/edge_canny.cpp
@@ -31,9 +31,39 @@ PNM* EdgeCanny::transform()
     EdgeSobel sobel(tmpImage);
     math::matrix<float>* horizGradient = sobel.rawHorizontalDetection();
     math::matrix<float>* vertiGradient = sobel.rawVerticalDetection();
+    float m[width][height];
+
     for(int x=0; x < width; x++){
         for(int y=0; y < height; y++){
-            //cos tu jest
+            m[x][y] = sqrt(pow((*horizGradient)(x,y),2) + pow((*vertiGradient)(x,y),2));
+        }
+    }
+
+    for(int x=0; x < width; x++){
+        for(int y=0; y < height; y++){
+            float orientation = fmod(atan2((*vertiGradient)(x,y),(*horizGradient)(x,y))/M_PI,M_PI) / M_PI * 180.0;
+
+            int pixels_to_compare[2];
+            if (orientation >= 22.5 && orientation < 67.5 || orientation < -112.5 && orientation > -157.5) {
+                pixels_to_compare[0] = m[x+1][y-1];
+                pixels_to_compare[1] = m[x-1][y+1];
+            } else if (orientation >= 67.5 && orientation < 112.5 || orientation < -67.5 && orientation > -112.5) {
+                pixels_to_compare[0] = m[x][y+1];
+                pixels_to_compare[1] = m[x][y-1];
+            } else if (orientation >= 112.5 && orientation < 157.5 || orientation < -22.5 && orientation > -67.5) {
+                pixels_to_compare[0] = m[x-1][y-1];
+                pixels_to_compare[1] = m[x+1][y+1];
+            } else if (orientation >= 157.5 && orientation < -157.5 || orientation >= -22.5 && orientation < 22.5) {
+                pixels_to_compare[0] = m[x+1][y];
+                pixels_to_compare[1] = m[x-1][y];
+            }
+
+            if (m[x][y] > pixels_to_compare[0] && m[x][y] > pixels_to_compare[1] && m[x][y] > upper_thresh) {
+                newImage->setPixel(x,y,Qt::white);
+            } else {
+                newImage->setPixel(x,y,Qt::black);
+            }
+
         }
     }
 
